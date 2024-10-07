b6b5489f9352bc87d5f7e4255ef4ec762027caa1
kitorn <kitorn@bk.ru>
2024-10-07 09:25:37 +0300

    finish hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+        const hash = crypto.createHash('sha256');
+        const input = fs.createReadStream(filePath);
+
+        for await (const chunk of input) {
+            hash.update(chunk);
+        }
+        const digest = hash.digest('hex');
+        console.log(digest);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await calculateHash();
