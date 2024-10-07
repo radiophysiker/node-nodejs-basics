b6e1c57f441f86428f9f064b70631a4684f35193
kitorn <kitorn@bk.ru>
2024-10-07 14:50:28 +0300

    finish stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'node:stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+    const readableStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+    await pipeline(
+        readableStream,
+        process.stdout
+    );
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'node:stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+    const writableStream = fs.createWriteStream(filePath, { encoding: 'utf-8' });
+    await pipeline(
+        process.stdin,
+        writableStream
+    );
 };
 
 await write();
