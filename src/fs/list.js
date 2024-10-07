b26af6f11a66ce1650c3d2741b1332fc6d473cc2
kitorn <kitorn@bk.ru>
2024-10-07 06:41:03 +0300

    fs end

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const errorFS = Error('FS operation failed');
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = path.join(__dirname, 'files');
+    try {
+        await fs.access(folderPath, fs.constants.F_OK);
+        const files = await fs.readdir(folderPath);
+        console.log(files);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw errorFS;
+        }
+        throw error;
+    }
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,27 @@
+import fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fileToRead = 'fileToRead.txt';
+
+const errorFS = Error('FS operation failed');
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', fileToRead);
+
+    try {
+        await fs.access(filePath, fs.constants.F_OK);
+        const content = await fs.readFile(filePath, 'utf-8');
+        console.log(content);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw errorFS;
+        }
+        throw error;
+    }
 };
 
 await read();
