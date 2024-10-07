d5ff0880bcbd7daf7bf02588b58f1a08dc77dd8a
kitorn <kitorn@bk.ru>
2024-10-07 06:33:27 +0300

    add remove

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,25 @@
+import fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fileToRemove = 'fileToRemove.txt';
+const errorFS = Error('FS operation failed');
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files' ,fileToRemove);
+
+    try {
+        await fs.access(filePath, fs.constants.F_OK);
+        await fs.unlink(filePath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw errorFS;
+        }
+        throw error;
+    }
 };
 
 await remove();
